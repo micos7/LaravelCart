3a2558cea584bb9bf1eb7fcc79d738455ff74b3a
micos7 <micos7@gmail.com>
2017-04-01 16:27:45 +0300

    Product index view

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -4,11 +4,17 @@
     <meta charset="utf-8">
     <title>@yield('title')</title>
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
-
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" />
+<link rel="stylesheet" href="{{ URL::to('src/css/app.css')}}" />
     @yield('styles')
   </head>
   <body>
-    @yield('content')
+@include('partials.header')
+<div class="container">
+  @yield('content')
+</div>
+
+
     <script
       src="https://code.jquery.com/jquery-1.12.4.min.js"
       integrity="sha256-ZosEbRLbNQzLpnKIkEdrPv7lOy9C27hHQ+Xp8a4MxAQ="
--- resources/views/shop/index.blade.php
+++ resources/views/shop/index.blade.php
@@ -4,5 +4,123 @@
 Laravel shopping cart
 @endsection
 @section('content')
-<h1>It works</h1>
+<div class="row">
+  <div class="col-sm-6 col-md-4">
+    <div class="thumbnail">
+      <img src="http://prodimage.images-bn.com/pimages/9781338099133_p0_v5_s1200x630.jpg" alt="..."  class="img-responsive">
+      <div class="caption">
+        <h3>Product title</h3>
+        <p class="description">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+          Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+          Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore
+          eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+        <div class="clearfix">
+          <div class="pull-left price">
+            12$
+          </div>
+          <a href="#" class="btn btn-success pull-right" role="button">Add to cart</a>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <div class="col-sm-6 col-md-4">
+    <div class="thumbnail">
+      <img src="http://prodimage.images-bn.com/pimages/9781338099133_p0_v5_s1200x630.jpg" alt="..."  class="img-responsive">
+      <div class="caption">
+        <h3>Product title</h3>
+        <p class="description">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+          Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+          Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore
+          eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+        <div class="clearfix">
+          <div class="pull-left price">
+            12$
+          </div>
+          <a href="#" class="btn btn-success pull-right" role="button">Add to cart</a>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <div class="col-sm-6 col-md-4">
+    <div class="thumbnail">
+      <img src="http://prodimage.images-bn.com/pimages/9781338099133_p0_v5_s1200x630.jpg" alt="..."  class="img-responsive">
+      <div class="caption">
+        <h3>Product title</h3>
+        <p class="description">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+          Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+          Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore
+          eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+        <div class="clearfix">
+          <div class="pull-left price">
+            12$
+          </div>
+          <a href="#" class="btn btn-success pull-right" role="button">Add to cart</a>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+
+
+
+<div class="row">
+  <div class="col-sm-6 col-md-4">
+    <div class="thumbnail">
+      <img src="http://prodimage.images-bn.com/pimages/9781338099133_p0_v5_s1200x630.jpg" alt="..."  class="img-responsive">
+      <div class="caption">
+        <h3>Product title</h3>
+        <p class="description">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+          Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+          Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore
+          eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+        <div class="clearfix">
+          <div class="pull-left price">
+            12$
+          </div>
+          <a href="#" class="btn btn-success pull-right" role="button">Add to cart</a>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <div class="col-sm-6 col-md-4">
+    <div class="thumbnail">
+      <img src="http://prodimage.images-bn.com/pimages/9781338099133_p0_v5_s1200x630.jpg" alt="..."  class="img-responsive">
+      <div class="caption">
+        <h3>Product title</h3>
+        <p class="description">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+          Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+          Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore
+          eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+        <div class="clearfix">
+          <div class="pull-left price">
+            12$
+          </div>
+          <a href="#" class="btn btn-success pull-right" role="button">Add to cart</a>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <div class="col-sm-6 col-md-4">
+    <div class="thumbnail">
+      <img src="http://prodimage.images-bn.com/pimages/9781338099133_p0_v5_s1200x630.jpg" alt="..."  class="img-responsive">
+      <div class="caption">
+        <h3>Product title</h3>
+        <p class="description">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+          Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+          Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore
+          eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+        <div class="clearfix">
+          <div class="pull-left price">
+            12$
+          </div>
+          <a href="#" class="btn btn-success pull-right" role="button">Add to cart</a>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
 @endsection
