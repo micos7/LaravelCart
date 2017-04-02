1ec6c0b003c3ee475e0767174499ae0d194abf9e
micos7 <micos7@gmail.com>
2017-04-02 18:52:17 +0300

    User signup

---
+++ resources/views/user/signup.blade.php
@@ -0,0 +1,31 @@
+@extends('layouts.master')
+
+@section('content')
+<div class="row">
+  <div class="col-md-4 col-md-offset-4">
+    <h1> Sign Up</h1>
+    @if(count($errors) > 0)
+    <div class="alert alert-danger">
+      <p>
+        {{ $error}}
+      </p>
+    </div>
+    @endif
+    <form action="{{ route('user.signup')}}" method="post">
+      <div class="form-group">
+        <label for="email">E-mail</label>
+        <input type="text" class="form-control" id="email" name="email" placeholder="">
+        <p class="help-block">Enter your email</p>
+      </div>
+      <div class="form-group">
+        <label for="password">Password</label>
+        <input type="password" class="form-control" id="password" name="password" placeholder="">
+        <p class="help-block">Enter your password</p>
+      </div>
+      <button type="submit" class="btn btn-primary">Sign Up</button>
+      {{ csrf_field()}}
+    </form>
+  </div>
+</div>
+
+@endsection
